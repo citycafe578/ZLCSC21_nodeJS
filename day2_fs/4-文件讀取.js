6d67f9332e7100f3a1bfbc0d7f9545987596a381
citycafe578 <chentim102@gmail.com>
2023-08-01 17:24:26 +0800

    Update 4-文件讀取.js

--- day2_fs/4-文件讀取.js
+++ day2_fs/4-文件讀取.js
@@ -2,4 +2,15 @@
 const fs = require('fs');
 
 //異步讀取
-fs.readFile();
+fs.readFile('./流式寫入.txt', (err,data) => {
+    if(err){
+        console.log('讀取失敗');
+        return;
+    }
+    //由漁會輸出ASCII碼，因此用toString轉換
+    console.log(data.toString());
+});
+
+//同步讀取
+let data = fs.readFileSync('./介紹.txt');
+console.log(data.toString());
