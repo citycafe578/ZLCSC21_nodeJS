3985801a815758d77722e7ad436172e610dfb006
citycafe578 <chentim102@gmail.com>
2023-08-02 14:48:18 +0800

    Update 6-文件複製.js

--- day2_fs/6-文件複製.js
+++ day2_fs/6-文件複製.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-//方法一readFile
+//方法一readFile(一次佔據全部暫存)
 let data = fs.readFileSync('./筆記.txt');
 fs.writeFileSync('./筆記_複製.txt', data);
 
-//方法二 流式
+//方法二 流式(占用較少資源)
 const rs = fs.createReadStream('./筆記.txt');
 const ws = fs.createWriteStream('./筆記_複製2.txt');
 
